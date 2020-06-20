625b99c307e01ac5fcb71dec76a930bf4e0677fd
viktoriazaharova <zaharova181114@gmail.com>
2020-06-20 08:46:59 +0300

    modal filter add

--- js/common.js
+++ js/common.js
@@ -241,3 +241,16 @@ $(".dec2").val($(".slider-range").slider("value"));
     });
 })(jQuery);
 
+$(document).ready(function () {
+    $(".js-tab-trigger").click(function () {
+        var id = $(this).attr('data-tab'),
+            content = $('.js-tab-content[data-tab="'+ id +'"]');
+
+        $('.js-tab-trigger.active').removeClass('active'); // 1
+        $(this).addClass('active'); // 2
+
+        $('.js-tab-content.active').removeClass('active'); // 3
+        content.addClass('active'); // 4
+    })
+});
+
